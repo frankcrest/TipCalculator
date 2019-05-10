f4a12ed5bb220550d9290adc8e3e0ac40d3c698e
Frank Chen <frankcrestview@hotmail.com>
2019-05-10 14:04:35 -0400

    As a user, I don't want to have to press a button every time I change a field

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate>
 
 @property (nonatomic, assign) float billAmount;
 @property (weak, nonatomic) IBOutlet UITextField *myTextField;
@@ -28,6 +28,9 @@
     // Do any additional setup after loading the view, typically from a nib.
     [self.notificationCenter addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
     [self.notificationCenter addObserver:self selector:@selector(keyboardDidHide:) name:UIKeyboardDidHideNotification object:nil];
+    
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(keyboarControl:)];
+    [self.view addGestureRecognizer:tapGesture];
 }
 
 -(NSNotificationCenter *)notificationCenter{
@@ -42,19 +45,15 @@
 
 -(float)calculateTip:(float)total{
     if (![self.tipPercentageTextField.text isEqualToString:@""]) {
-        NSLog(@"textfield");
         self.tipPercentage = [self.tipPercentageTextField.text floatValue] / 100;
+        
     } else if (self.tipSlider.value){
-        NSLog(@"slider");
         self.tipPercentage = self.tipSlider.value / 100;
+    
     } else{
-        NSLog(@"default");
+       
         self.tipPercentage = 0.15;
     }
-
-    [self.myTextField resignFirstResponder];
-    [self.tipPercentageTextField resignFirstResponder];
-    
     return total * self.tipPercentage;
 }
 
@@ -69,12 +68,37 @@
 
 - (IBAction)adjustTipPercentage:(UISlider *)sender {
     self.tipPercentage = sender.value;
+    float billAmount = [self.myTextField.text floatValue];
+    float tipAmount = billAmount * self.tipPercentage / 100;
+    self.tipLabel.text = [NSString stringWithFormat:@"$%0.2f", tipAmount];
 }
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string{
+    if (textField == self.myTextField) {
+        NSString* newString = [textField.text stringByReplacingCharactersInRange:range withString:string];
+        float totalAmount = [newString floatValue];
+        NSString* tipAmount = [NSString stringWithFormat:@"$%0.2f",[self calculateTip:totalAmount]];
+        self.tipLabel.text = tipAmount;
+    } else if (textField == self.tipPercentageTextField){
+        NSString* newTip = [textField.text stringByReplacingCharactersInRange:range withString:string];
+        float tipPercentage = [newTip floatValue] / 100;
+        float totalAmount = [self.myTextField.text floatValue];
+        float tipAmount = tipPercentage * totalAmount;
+        self.tipLabel.text = [NSString stringWithFormat:@"$%0.2f", tipAmount];
+    }
+    return YES;
+}
+
+
 
 - (void) dealloc{
     [self.notificationCenter removeObserver:self];
 }
 
+-(void)keyboarControl:(UITapGestureRecognizer*)sender{
+    [self.myTextField resignFirstResponder];
+    [self.tipPercentageTextField resignFirstResponder];
+}
 
 @end
 
