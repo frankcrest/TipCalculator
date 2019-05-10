f4b2d1e3fd2638c7e1614c7ac568871a27f91739
Frank Chen <frankcrestview@hotmail.com>
2019-05-10 13:11:43 -0400

    As a user, I want to be able to easily adjust my tip percentage

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -14,6 +14,10 @@
 @property (weak, nonatomic) IBOutlet UITextField *myTextField;
 @property (weak, nonatomic) IBOutlet UILabel *tipLabel;
 @property (weak, nonatomic) IBOutlet UITextField *tipPercentageTextField;
+@property (nonatomic,strong) NSNotificationCenter* notificationCenter;
+@property (weak, nonatomic) IBOutlet UIView *containerView;
+@property (weak, nonatomic) IBOutlet UISlider *tipSlider;
+@property (nonatomic, assign) float tipPercentage;
 
 @end
 
@@ -22,16 +26,53 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self.notificationCenter addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
+    [self.notificationCenter addObserver:self selector:@selector(keyboardDidHide:) name:UIKeyboardDidHideNotification object:nil];
 }
+
+-(NSNotificationCenter *)notificationCenter{
+    return [NSNotificationCenter defaultCenter];
+}
+
 - (IBAction)calculateTipButtonPressed:(id)sender {
     float totalAmount = [self.myTextField.text floatValue];
-    NSString* tipAmount = [NSString stringWithFormat:@"$%f",[self calculateTip:totalAmount]];
+    NSString* tipAmount = [NSString stringWithFormat:@"$%0.2f",[self calculateTip:totalAmount]];
     self.tipLabel.text = tipAmount;
 }
 
 -(float)calculateTip:(float)total{
-    float tipPercentage = [self.tipPercentageTextField.text floatValue] ? [self.tipPercentageTextField.text floatValue] / 100 : 0.5;
-    return total * tipPercentage;
+    if (![self.tipPercentageTextField.text isEqualToString:@""]) {
+        NSLog(@"textfield");
+        self.tipPercentage = [self.tipPercentageTextField.text floatValue] / 100;
+    } else if (self.tipSlider.value){
+        NSLog(@"slider");
+        self.tipPercentage = self.tipSlider.value / 100;
+    } else{
+        NSLog(@"default");
+        self.tipPercentage = 0.15;
+    }
+
+    [self.myTextField resignFirstResponder];
+    [self.tipPercentageTextField resignFirstResponder];
+    
+    return total * self.tipPercentage;
+}
+
+- (void)keyboardWillShow:(NSNotification *)notification{
+    self.containerView.center = CGPointMake(self.view.center.x, self.view.center.y - 100);
+}
+
+- (void)keyboardDidHide:(NSNotification *)notification{
+    NSLog(@"hide");
+    //self.containerView.center = CGPointMake(self.view.center.x, self.view.center.y);
+}
+
+- (IBAction)adjustTipPercentage:(UISlider *)sender {
+    self.tipPercentage = sender.value;
+}
+
+- (void) dealloc{
+    [self.notificationCenter removeObserver:self];
 }
 
 
