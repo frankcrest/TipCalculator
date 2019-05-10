3d747a109b7804fed63ae4d656a8590e18ac83a9
Frank Chen <frankcrestview@hotmail.com>
2019-05-10 11:23:59 -0400

    Initial Commit

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Frank Chen on 2019-05-10.
+//  Copyright © 2019 Frank Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
